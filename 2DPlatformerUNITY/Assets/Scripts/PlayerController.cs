9f4a6473001d0227625ff782301dca04b8590cdd
Clamel05 <caml@bell.net>
2024-11-26 13:19:40 -0500

    Improved the horizontal movement

--- 2DPlatformerUNITY/Assets/Scripts/PlayerController.cs
+++ 2DPlatformerUNITY/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,23 @@
+using System;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
+
+
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D body;
     Rigidbody2D rigidPlayer;
-    public float speed = 1f;
+
+    public float maxSpeed = 20f;
+    public float accelerationTime = 0.25f;
+    public float deccelerationTime = 0.15f;
+
+    public float accelerationRate;
+    public float deccelerationRate;
+
     [SerializeField] private float groundCheckDistance = 1f;
     [SerializeField] private LayerMask jumpLayerMask;
     //[SerializeField] private ForceMode2D forceMode;
@@ -16,19 +27,32 @@ public class PlayerController : MonoBehaviour
     public float maxHeight = 5f;
 
 
-    //public FacingDirection GetFacingDirection;
-
     public enum FacingDirection
     {
         left, right
 
     }
 
+
+    //public FacingDirection GetFacingDirection;
+
+
+
+    private FacingDirection currentDirection = FacingDirection.right;
+
+    private Vector2 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (rigidPlayer == null)
-        rigidPlayer = GetComponent<Rigidbody2D>();
+        //if (rigidPlayer == null)
+        //rigidPlayer = GetComponent<Rigidbody2D>();
+
+        body.gravityScale = 0;
+
+        accelerationRate = maxSpeed / accelerationTime;
+        deccelerationRate = maxSpeed / deccelerationTime;
+
     }
 
     // Update is called once per frame
@@ -44,24 +68,53 @@ public class PlayerController : MonoBehaviour
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
+        playerInput.x = Input.GetAxisRaw("Horizontal");  //Prof way of doing this
         MovementUpdate(playerInput);
+
+        body.velocity = velocity;
 
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
+        //Prof way
+        if (playerInput.x < 0)
+            currentDirection = FacingDirection.left;
+        else if (playerInput.x > 0)
+            currentDirection = FacingDirection.right;
+
+        if(playerInput.x != 0)
+        {
+            velocity.x += accelerationRate * playerInput.x * Time.deltaTime;
+            velocity.x = Mathf.Clamp(velocity.x, -maxSpeed, maxSpeed);
+        }
+        else
+        {
+            if(velocity.x > 0)
+            {
+                velocity.x -= deccelerationRate * Time.deltaTime;
+                velocity.x = Mathf.Max(velocity.x, 0);
+            }
+            else if (velocity.x < 0)
+            {
+                velocity.x += deccelerationRate * Time.deltaTime;
+                velocity.x = Mathf.Min(velocity.x, 0);
+            }
+        }
+
+
 
         //My first attempt at task1
-        if (Input.GetKey(KeyCode.A))
+        /*if (Input.GetKey(KeyCode.A))
         {
             playerInput = new Vector2(-1, 0);
-            rigidPlayer.AddForce(playerInput * speed);
+            rigidPlayer.AddForce(playerInput * maxSpeed);
         }
 
         if (Input.GetKey(KeyCode.D))
         {
             playerInput = new Vector2(1, 0);
-            rigidPlayer.AddForce(playerInput * speed);
+            rigidPlayer.AddForce(playerInput * maxSpeed);
         }
 
         //Jumping - Task 1 Journal 7
@@ -71,12 +124,16 @@ public class PlayerController : MonoBehaviour
             rigidPlayer.AddForce(playerInput * maxHeight);
             Debug.Log("Jump");
         }
-
+        */
         
     }
 
     public bool IsWalking()
     {
+        return velocity.x != 0;
+
+
+
         /*
         if ((Input.GetKey(KeyCode.A)) || (Input.GetKey(KeyCode.D)))
         {
@@ -89,7 +146,6 @@ public class PlayerController : MonoBehaviour
             return false;
         }
         */
-        return false;
     }
     public bool IsGrounded()
     {
@@ -115,6 +171,6 @@ public class PlayerController : MonoBehaviour
 
     public FacingDirection GetFacingDirection()
     {
-        return FacingDirection.left;
+        return currentDirection;
     }
 }
